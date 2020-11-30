4a1ceabcf20c473d769a6668841c957f0b2ad15d
Ilya <mitin85@list.ru>
2020-11-30 19:22:17 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+{let second = 2;}
+const pi = 3.14;
+
+
+console.log(4/0);
+console.log('string'/6);
+
+let persone = {
+    name:"John",
+    age:25,
+    isMarryed:false
+}
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.png','apple.png'];
+console.log(arr[0]);
+//alert("Hello World!!!");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+let answer = +prompt("Есть ли вам 18?","Да");
+console.log(answer);
