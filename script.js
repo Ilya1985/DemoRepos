0e403323f7fc036fab76f2795ddbad50d083aafb
Ilya <mitin85@list.ru>
2020-12-01 16:51:47 +0700

    Work 1

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 'use strict';
 
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
 
-var leftBorderWidth = 1;
-{let second = 2;}
-const pi = 3.14;
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
+    appData.expenses.a1 = a2;
+    appData.expenses.a3 = a4;
 
-console.log(4/0);
-console.log('string'/6);
+    alert(appData.budget / 30);
 
-let persone = {
-    name:"John",
-    age:25,
-    isMarryed:false
-}
-
-console.log(persone["name"]);
-
-let arr = ['plum.png','orange.png','apple.png'];
-console.log(arr[0]);
-//alert("Hello World!!!");
-// let answer = confirm("Are you here?");
-// console.log(answer);
-
-let answer = +prompt("Есть ли вам 18?","Да");
-console.log(answer);
